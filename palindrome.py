16e2da2ec2e73251e50cd725d92b24057ccdf075
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2019-02-19 19:54:14 -0800

    I got bored

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+import re
+
+def pal(word):
+    word.lower()
+    first = 0
+    last = len(word) - 1
+
+    # Loop while the first position is less than the last position
+    while (first < last):
+
+        # Both condition statements check for white spaces
+        if re.search("\W", word[first]):
+            first += 1
+        if re.search("\W", word[last]):
+            last -= 1
+        
+        # returns false if two letters done match
+        if word[first] != word[last]:
+            return print(word + " is not a palindrome")
+        
+        first += 1
+        last -= 1
+
+pal(input("Enter a word: "))
